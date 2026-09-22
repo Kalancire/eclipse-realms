3ba53e851e07434db2fa423d19ad78729ae91fa7
Cirejanrey <villanuevacire0@gmail.com>
2026-09-22 19:07:54 +0800

    Fix import statement for Node class in CharacterModelFactory

--- core/src/main/java/com/eclipserealms/game/graphics/CharacterModelFactory.java
+++ core/src/main/java/com/eclipserealms/game/graphics/CharacterModelFactory.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.graphics.g3d.Node;
+   import com.badlogic.gdx.graphics.g3d.model.Node;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
